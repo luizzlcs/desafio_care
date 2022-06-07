6d68b5c1ad2d7599a7dbad4b6d8355499d6b2bac
Luiz Carlos <luizzlcs@hotmail.com>
2022-06-07 19:52:02 -0300

    Remove SetState end add provider

--- lib/app/pages/list_client/card_list_view/card_internal.dart
+++ lib/app/pages/list_client/card_list_view/card_internal.dart
@@ -1,6 +1,8 @@
+import 'package:desafio_care/app/pages/list_client/card_list_view/classific_clintion.dart';
 import 'package:desafio_care/firebase_firestore/client.dart';
 import 'package:desafio_care/firebase_firestore/data_base_client.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CardInternal extends StatefulWidget {
   const CardInternal({Key? key}) : super(key: key);
@@ -12,62 +14,20 @@ class CardInternal extends StatefulWidget {
 class _CardInternalState extends State<CardInternal> {
   var clientsData = DataBaseClient();
 
-  var order = 0;
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: (() {
-                  setState(() {
-                    order = 0;
-                  });
-                }),
-                child: Text(
-                  'Default',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: order == 0 ? Colors.red : Colors.black),
-                ),
-              ),
-              GestureDetector(
-                onTap: (() {
-                  setState(() {
-                    order = 1;
-                  });
-                }),
-                child: Text(
-                  'A-z',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: order == 1 ? Colors.red : Colors.black),
-                ),
-              ),
-              GestureDetector(
-                onTap: (() {
-                  setState(() {
-                    order = 2;
-                  });
-                }),
-                child: Text(
-                  'Z-a',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: order == 2 ? Colors.red : Colors.black),
-                ),
-              ),
-            ],
-          ),
+          
           SizedBox(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             child: StreamBuilder<List<Client>>(
-                stream: switchFind(clientsData, order),
+                stream: switchFind(
+                  clientsData,
+                  (context.select((ClassificationClient c) => c.order)),
+                ),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
                     return const CircularProgressIndicator(
